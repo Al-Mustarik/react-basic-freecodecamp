e0764167f015eda137fa12c2324d1083a5a336cb
Al Mustarik <almustarik@gmail.com>
2020-12-16 22:04:31 +0600

    added static data

---
+++ src/books.js
@@ -0,0 +1,19 @@
+export const books = [
+  { id:1,
+    img:'https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg',
+    title:'A Promised Land',
+    author: 'Barack Obama',
+  },
+
+  { id:2,
+    img:'https://images-na.ssl-images-amazon.com/images/I/41U3yoF6sXL._SX427_BO1,204,203,200_.jpg',
+    title:'Greenlights',
+    author: 'Matthew McConaughey',
+  },
+
+  { id:3,
+    img:'https://images-na.ssl-images-amazon.com/images/I/51XxSZxKoDL._SX331_BO1,204,203,200_.jpg',
+    title:'A Time for Mercy (Jake Brigance)',
+    author: 'John Grisham',
+  },
+]
