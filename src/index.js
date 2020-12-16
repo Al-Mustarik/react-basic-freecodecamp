a2adb431a934517220e7b40f1b24b5485df49eb0
Al Mustarik <almustarik@gmail.com>
2020-12-16 10:49:22 +0600

    simple list

--- src/index.js
+++ src/index.js
@@ -5,51 +5,45 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 // setup variables
-const firstBook = {
-  img:'https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg',
-  title:'A Promised Land',
-  author: 'Barack Obama',
-}
-const secondBook = {
-  img:'https://images-na.ssl-images-amazon.com/images/I/41U3yoF6sXL._SX427_BO1,204,203,200_.jpg',
-  title:'Greenlights',
-  author: 'Matthew McConaughey',
-}
+// list 
+// const books = [
+//   {
+//     img:'https://images-na.ssl-images-amazon.com/images/I/41L5qgUW2fL._SX327_BO1,204,203,200_.jpg',
+//     title:'A Promised Land',
+//     author: 'Barack Obama',
+//   },
 
+//   {
+//     img:'https://images-na.ssl-images-amazon.com/images/I/41U3yoF6sXL._SX427_BO1,204,203,200_.jpg',
+//     title:'Greenlights',
+//     author: 'Matthew McConaughey',
+//   },
+// ]
+
+const names = ['jhon', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>
+});
+
+console.log(newNames);
 function BookList(){
-  return(
-    <section className = "bookList">
-      <Book 
-      img={firstBook.img} 
-      title={firstBook.title} 
-      author={firstBook.author} 
-      >
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
-          Nostrum dolorum cum nisi error possimus laborum praesentium 
-          aspernatur quia placeat porro!</p>
-      </Book>
-      <Book 
-      img={secondBook.img} 
-      title={secondBook.title} 
-      author={secondBook.author} 
-      />
-    </section>
+  return (
+    <section className="bookList"> {newNames} </section>
   );
 }
 
 
-const Book = (props) => {
-  const { img, title, author } = props;
-  console.log(props);
-  return (
-    <article className = "book">
-      <img src={img} alt=""/>
-      <h2> {title} </h2>
-      <h4> {author} </h4>
-      {props.children}
-    </article>
-  );
-};
+// const Book = (props) => {
+//   const { img, title, author } = props;
+//   console.log(props);
+//   return (
+//     <article className = "book">
+//       <img src={img} alt=""/>
+//       <h2> {title} </h2>
+//       <h4> {author} </h4>
+//     </article>
+//   );
+// };
 
 
 // < /> ==> self closing tag
